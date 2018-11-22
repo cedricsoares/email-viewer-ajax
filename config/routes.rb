0489eb69aa80df739d220e56180d7b5d4d5d445a
Cedric Soares <cedricsoares@me.com>
2018-11-22 11:29:31 +0100

    Add Email controller, emails model and seed tab

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
+  get 'emails/index'
+  get 'emails/destroy'
+  get 'email/index'
+  get 'email/destroy'
   resources :gossips, only: [:index, :create, :new]
-  root to: "gossips#index"
+  root 'email#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
