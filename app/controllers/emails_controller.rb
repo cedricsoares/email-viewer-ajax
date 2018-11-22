76245df07b7fee72f0f2acc43b3763a27dd0df9f
Snakultime <snakultime@hotmail.fr>
2018-11-22 18:45:28 +0100

    add destroy files and link

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,23 +1,33 @@
 class EmailsController < ApplicationController
   def index
-  	@emails = Email.all 
+    @emails = Email.all
 
   end
 
   def show
-  	@email = Email.find(params[:id])
+    @email = Email.find(params[:id])
 
     puts @email
 
     puts "Axel drague la serveuse ! Dommage elle parle pas français"
 
-  	  respond_to do |f|
-	    f.html { redirect_to email_path }
-	    f.js
-  	end 
+    respond_to do |f|
+      f.html { redirect_to email_path }
+      f.js
+    end
   end
 
 
   def destroy
+    @emails = Email.all
+    @email = Email.find(params[:id]).destroy
+
+    puts @email.id
+
+    respond_to do |f|
+      f.html { redirect_to email_url }
+      f.js
+    end
   end
+
 end
